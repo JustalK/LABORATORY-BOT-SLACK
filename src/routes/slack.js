97477ff0d87f5a69b2c3e5fab233b366191acd3d
justalk <justal.kevin@gmail.com>
2022-07-17 12:16:11 +0200

    [FEATURE] Adding the message sent

--- src/routes/slack.js
+++ src/routes/slack.js
@@ -1,12 +1,31 @@
+/**
+* The endpoint of managing the slack event
+* @module routes/slack
+*/
+'use strict'
 const eventsApi = require('@slack/events-api')
-const slackEvents = eventsApi.createEventAdapter(process.env.SIGNING_SECRET || '993adb4ac097aadcad8b304ab276d861')
+const slackEvents = eventsApi.createEventAdapter(process.env.SIGNING_SECRET)
 const { WebClient, LogLevel } = require('@slack/web-api')
 
-const client = new WebClient(process.env.BOT_TOKEN || 'xoxb-3230192996481-3802065341878-a23cOwV1W7sj3LlrkDCIMPE6', {
+const client = new WebClient(process.env.BOT_TOKEN, {
   logLevel: LogLevel.DEBUG
 })
 
-app.use('/', slackEvents.expressMiddleware())
+const writeMessageInChannel = async (event, text) => {
+  return client.chat.postMessage({
+    channel: event.channel,
+    text
+  })
+}
+
 slackEvents.on('message', async (event) => {
-  console.log(event)
+  if (!event.subtype && !event.bot_id) {
+    if (event.text === '~nudge') {
+      writeMessageInChannel(event, `No problem! I know where you are:\nchannel ID: ${event.channel}\n\nI dont have a nudge for you yet!`)
+    } else {
+      writeMessageInChannel(event, 'Wrong command!\n~nudge: For getting a message')
+    }
+  }
 })
+
+module.exports = slackEvents.expressMiddleware()
